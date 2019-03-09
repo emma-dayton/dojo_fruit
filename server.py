86ca8b6b5072eaa121a6fe178d8d35031c137adb
Emma Dayton <emma.dayton85@gmail.com>
2019-03-08 20:32:47 -0800

    get all info to display in checkout page, including date/time of purchase formatted nicely, resized fruit pics on fruit page, did a lot in the server.py file

--- server.py
+++ server.py
@@ -1,18 +1,38 @@
 from flask import Flask, render_template, request, redirect
-app = Flask(__name__)  
+from datetime import datetime
 
-@app.route('/')         
+app = Flask(__name__)
+
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
+@app.route('/checkout', methods=['POST'])
 def checkout():
-    print(request.form)
-    return render_template("checkout.html")
+    fruits = {
+    'straw': str(request.form.get('strawberry')),
+    'rasp': str(request.form.get('raspberry')),
+    'apple': str(request.form.get('apple')),
+    'black': str(request.form.get('blackberry')),
+    }
+    num = int(fruits['straw']) + int(fruits['rasp']) + int(fruits['apple']) + int(fruits['black'])
+    now = datetime.now()
+    year = now.strftime("%Y")
+    month = now.strftime("%B")
+    day = now.strftime("%-d")
+    time = now.strftime("%I:%M:%S %p")
+    date = ' '.join([day, month, year, 'at', time])
+    order = f'We ordered {num} items for you and charged your student account on {date}.'
+    fname = request.form['first_name']
+    lname = request.form['last_name']
+    student_id = request.form['student_id']
 
-@app.route('/fruits')         
+    print(f'Charging {fname} {lname} for {num} items of fruit')
+    return render_template("checkout.html", order=order, fruits=fruits, fname=fname, lname=lname, student_id=student_id)
+
+@app.route('/fruits')
 def fruits():
     return render_template("fruits.html")
 
-if __name__=="__main__":   
-    app.run(debug=True)    
+if __name__=="__main__":
+    app.run(debug=True)
